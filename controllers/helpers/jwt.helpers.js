37426a406ecfd55ce9e0e4b3e3c135b7dbc38a63
nixonamado <nixonamado00@gmail.com>
2024-01-20 14:18:17 -0500

    feat: :rocket: JWT finished
    finished implementing JWT in its jwt.helpers file and tested it on index user 's path conection

--- controllers/helpers/jwt.helpers.js
+++ controllers/helpers/jwt.helpers.js
@@ -2,7 +2,7 @@
 import dotenv from 'dotenv'
 import passport from 'passport'
 import { SignJWT, jwtVerify } from 'jose'
-import { strategy as BearerStrategy } from 'passport-http-bearer'
+import { Strategy as BearerStrategy } from 'passport-http-bearer'
 
 // llamar variables de entorno
 dotenv.config('../')
@@ -42,3 +42,35 @@ export function requireRole (role) {
   }
 }
 
+export async function validateToken (req, res, next) {
+  try {
+    const encoder = new TextEncoder()
+    const { payload } = await jwtVerify(
+      req.headers.authorization.split(' ')[1],
+      encoder.encode(process.env.JWT_KEY)
+    )
+    if (payload.role === 'admin' || payload.role === 'student') {
+      req.user = payload
+      return next()
+    } else {
+      res.status(404).json({ status: 404, message: 'Not Found' })
+    }
+  } catch (err) {
+    console.error(err)
+    return res.status(400).json({ status: 401, message: 'Unauthorized' })
+  }
+}
+
+export async function createToken (req, res) {
+  const encoder = new TextEncoder()
+  if (req.body.role === 'admin' || req.body.role === 'student') {
+    const jwtConstructor = await new SignJWT(req.body)
+      .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
+      .setIssuedAt()
+      .setExpirationTime('1h')
+      .sign(encoder.encode(process.env.JWT_KEY))
+    res.status(200).json({ status: 200, token: jwtConstructor })
+  } else {
+    res.status(400).json({ status: 400, message: 'Invalid Credentials required' })
+  }
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import { createToken, validateToken } from './controllers/helpers/jwt.helpers.js'
 import { boomErrorHandler, errorHandler, logErrors } from './middlewares/error.handler.js'
 import { user } from './controllers/Routers/user.route.js'
 const app = express()
@@ -14,7 +15,9 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('hola mundo')
 })
-app.use('/user', user)
+app.get('/token', createToken)
+
+app.use('/user', validateToken, user)
 // Middlewares
 app.use(morgan('dev'))
 app.use(cors({ origin: '*' }))
@@ -22,7 +25,6 @@ app.use(cors({ origin: '*' }))
 app.use(logErrors)
 app.use(boomErrorHandler)
 app.use(errorHandler)
-
 // Listen
 app.listen(app.get('port'), () => {
   console.log('my local host = http://localhost:' + app.get('port'))
